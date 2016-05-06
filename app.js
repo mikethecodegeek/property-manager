96c31fdaaa846919d94073e4a793948c4f219d86
Admin <Admin@Admins-MacBook-Pro-2.local>
2016-05-05 19:39:19 -0700

    updated mongo connection config

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
 //var MONGODB_URI = 'mongodb://<example>:<example>@ds013222.mlab.com:13222/properties'
-const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/mongo-express';
+const MONGOURL = process.env.MONGOLAB_CHARCOAL_URI || 'mongodb://localhost/mongo-express';
 mongoose.connect(MONGOURL, err => {
     console.log(err || 'Connected to MongoDB')
 });
